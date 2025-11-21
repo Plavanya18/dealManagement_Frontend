78ea3a4cf2218b5905016b40b3df086e91d10358
Plavanya18 <prakashlavanya18@gmail.com>
2025-11-21 13:10:56 +0530

    stat card fixed

--- src/components/StatsCard/StatsCard.jsx
+++ src/components/StatsCard/StatsCard.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 function StatsCard({ title, value, subChange, icon }) {
   return (
-    <div  className="w-[220px] h-[120px] bg-white rounded-xl border border-[#E5E5E5] shadow-sm p-4 flex flex-col justify-center gap-2">
-      
-      <div className="flex justify-between items-start p-6">
-        <h4 className="text-[16px] font-semibold text-black">{title}</h4>
+    <div className="w-[220px] h-[120px] rounded-xl border bg-white border-[#E5E5E5] shadow-sm p-4 flex flex-col justify-center items-start gap-2 text-center">
+
+      <div className="flex items-center gap-2">
+        <h4 className="text-[16px] font-normal text-black">{title}</h4>
 
         {icon && (
-          <img src={icon} alt="icon" className="w-6 h-6" />
+          <img src={icon} alt="icon" className="w-5 h-5" />
         )}
       </div>
 
-      <p className="text-[16px] font-bold text-black mt-1">{value}</p>
+      <p className="text-[16px] font-bold text-black">{value}</p>
 
-      <p className="text-[15px] text-[#6c757d] mt-1">{subChange}</p>
+      <p className="text-[15px] text-[#565656]">{subChange}</p>
+
     </div>
+
   );
 }
 
