65d7f697f3e1d31f61705a78b75949ff3b79b322
Plavanya18 <prakashlavanya18@gmail.com>
2025-11-25 19:59:27 +0530

    blur

--- src/pages/UserManagement/ListUser.jsx
+++ src/pages/UserManagement/ListUser.jsx
@@ -190,7 +190,7 @@ function ListUser() {
 
             <Toast show={toast.show} message={toast.message} type={toast.type} />
 
-            <div className={showAddUser ? "filter blur-[0.5px] w-full" : ""}>
+            <div className={`${showAddUser || toast.show  || viewUser || NotificationCard ? "backdrop-blur-sm backdrop-brightness-90 w-full" : ""}`}>
                 <Navbar />
                 <div className="flex">
                     <div className="w-[220px]">
