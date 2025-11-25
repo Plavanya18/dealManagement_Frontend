10d7bcdadfb9c56fb9f66c01d931238dd6523173
Plavanya18 <prakashlavanya18@gmail.com>
2025-11-25 12:56:31 +0530

    view user page with phone

--- src/pages/UserManagement/ViewUser.jsx
+++ src/pages/UserManagement/ViewUser.jsx
@@ -110,7 +110,7 @@ function ViewUser({ userId, onClose }) {
                                         readOnly
                                         className="sr-only peer"
                                     />
-                                    <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-yellow-500 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+                                    <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-yellow-500 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                                 </label>
                             </div>
                         </div>
