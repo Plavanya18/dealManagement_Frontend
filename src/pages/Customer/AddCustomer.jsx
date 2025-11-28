d5f75d6d639c70b09feaa13724c63c5edd1ae591
Plavanya18 <prakashlavanya18@gmail.com>
2025-11-28 16:03:42 +0530

    bank column add, remove

--- src/pages/Customer/AddCustomer.jsx
+++ src/pages/Customer/AddCustomer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function AddCustomer({ onClose }) {
-    const [customerType, setCustomerType] = useState("retail"); // retail by default
+    const [customerType, setCustomerType] = useState("retail");
     const [formData, setFormData] = useState({
         name: "",
         idNumber: "",
@@ -11,10 +11,45 @@ function AddCustomer({ onClose }) {
         email: "",
         phone: "",
     });
+    const [bankAccounts, setBankAccounts] = useState([
+    {
+        accountHolderName: "",
+        bankName: "",
+        accountNumber: "",
+        currency: "",
+        accountType: "",
+    }
+    ]);
+
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleBankAccountChange = (index, e) => {
+        const { name, value } = e.target;
+        const updated = [...bankAccounts];
+        updated[index][name] = value;
+        setBankAccounts(updated);
+    };
+
+    const addNewAccount = () => {
+    setBankAccounts([
+        ...bankAccounts,
+        {
+        accountHolderName: "",
+        bankName: "",
+        accountNumber: "",
+        currency: "",
+        accountType: "",
+        }
+    ]);
+    };
+
+    const removeAccount = (index) => {
+        if (bankAccounts.length === 1) return; 
+        setBankAccounts(bankAccounts.filter((_, i) => i !== index));
     };
 
     const handleFileChange = (e) => {
@@ -147,82 +182,108 @@ function AddCustomer({ onClose }) {
                         </div>
                         </div>
                         
-                        <div>
                         <h3 className="font-semibold mb-2">Bank Accounts</h3>
-                        <div className="grid grid-cols-2 gap-4">
-                            <div className="flex flex-col">
-                            <span className="text-gray-500 text-sm mb-1">Account Holder Name</span>
-                            <input
-                                type="text"
-                                name="accountHolderName"
-                                placeholder="Enter Name of Account Holder"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                                required
-                            />
+
+                        {bankAccounts.map((account, index) => (
+                        <div key={index} className="mb-10 relative">
+
+                            {bankAccounts.length > 1 && (
+                            <div className="flex justify-end mb-1">
+                                <button
+                                type="button"
+                                onClick={() => removeAccount(index)}
+                                className="px-3 py-2 rounded-md border border-red-500 text-red-500 hover:bg-red-50 text-sm font-semibold"
+                                >
+                                Remove Account
+                                </button>
                             </div>
-                            <div className="flex flex-col">
-                            <span className="text-gray-500 text-sm mb-1">Bank Name</span>
-                            <input
-                                type="text"
-                                name="bankName"
-                                placeholder="Enter Name of Bank"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                                required
-                            />
+                            )}
+
+                            <div className="p-4 rounded-xl bg-white">
+
+                            <div className="grid grid-cols-2 gap-4">
+                                <div className="flex flex-col">
+                                <span className="text-gray-500 text-sm mb-1">Account Holder Name</span>
+                                <input
+                                    type="text"
+                                    name="accountHolderName"
+                                    placeholder="Enter Name of Account Holder"
+                                    value={account.accountHolderName}
+                                    onChange={(e) => handleBankAccountChange(index, e)}
+                                    className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                    required
+                                />
+                                </div>
+
+                                <div className="flex flex-col">
+                                <span className="text-gray-500 text-sm mb-1">Bank Name</span>
+                                <input
+                                    type="text"
+                                    name="bankName"
+                                    placeholder="Enter Name of Bank"
+                                    value={account.bankName}
+                                    onChange={(e) => handleBankAccountChange(index, e)}
+                                    className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                    required
+                                />
+                                </div>
+                            </div>
+
+                            <div className="grid grid-cols-3 mt-4 gap-4">
+                                <div className="flex flex-col">
+                                <span className="text-gray-500 text-sm mb-1">Account Number</span>
+                                <input
+                                    type="text"
+                                    name="accountNumber"
+                                    placeholder="Enter Account Number"
+                                    value={account.accountNumber}
+                                    onChange={(e) => handleBankAccountChange(index, e)}
+                                    className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                    required
+                                />
+                                </div>
+
+                                <div className="flex flex-col">
+                                <span className="text-gray-500 text-sm mb-1">Currency</span>
+                                <input
+                                    type="text"
+                                    name="currency"
+                                    placeholder="Select Currency"
+                                    value={account.currency}
+                                    onChange={(e) => handleBankAccountChange(index, e)}
+                                    className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                    required
+                                />
+                                </div>
+
+                                <div className="flex flex-col">
+                                <span className="text-gray-500 text-sm mb-1">Account Type</span>
+                                <input
+                                    type="text"
+                                    name="accountType"
+                                    placeholder="Select Account Type"
+                                    value={account.accountType}
+                                    onChange={(e) => handleBankAccountChange(index, e)}
+                                    className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                                    required
+                                />
+                                </div>
+                            </div>
+
                             </div>
                         </div>
-                         <div className="grid grid-cols-3 mt-4 gap-4">
-                            <div className="flex flex-col">
-                            <span className="text-gray-500 text-sm mb-1">Account Number</span>
-                            <input
-                                type="text"
-                                name="accountNumber"
-                                placeholder="Select Account Number"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                                required
-                            />
-                            </div>
-                            <div className="flex flex-col">
-                            <span className="text-gray-500 text-sm mb-1">Currency</span>
-                            <input
-                                type="text"
-                                name="currency"
-                                placeholder="Select Currency"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                                required
-                            />
-                            </div>
-                             <div className="flex flex-col">
-                            <span className="text-gray-500 text-sm mb-1">Account Type</span>
-                            <input
-                                type="text"
-                                name="accountType"
-                                placeholder="Select Account Type"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                                required
-                            />
-                            </div>
-                        </div>
-                         <div className="flex justify-end space-x-3 mt-4">
+                        ))}
+
+                        <div className="flex justify-end">
                             <button
                                 type="button"
                                 className="px-4 py-2 rounded-md border border-yellow-500 text-yellow-600 hover:bg-yellow-50"
-                                onClick={onClose}
+                                onClick={addNewAccount}
                             >
                                 Add Account
                             </button>
                         </div>
-                        </div>
+
                         <div className="flex justify-end space-x-3 mt-4">
                         <button
                             type="button"
