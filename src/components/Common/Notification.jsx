903ae9b53ec58fc54dea45d7cd249ad83daf90ea
Plavanya18 <prakashlavanya18@gmail.com>
2025-11-28 12:04:13 +0530

    button placement with icon

--- src/components/Common/Notification.jsx
+++ src/components/Common/Notification.jsx
@@ -5,6 +5,8 @@ import deleteIcon from "../../assets/delete.svg";
 import resetIcon from "../../assets/reset_lock.svg";
 import redShadeIcon from "../../assets/red_shade.svg";
 import greenShadeIcon from "../../assets/green_shade.svg";
+import rejectIcon from "../../assets/reject.svg";
+import verifyIcon from "../../assets/verify.svg";
 
 function NotificationCard({ confirmModal, onConfirm, onCancel }) {
     if (!confirmModal.open) return null;
@@ -20,6 +22,10 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
             ? "Activate Account"
             : actionType === "resetPassword"
             ? "Send Link"
+            : actionType === "reject"
+            ? "Reject Account"
+            : actionType === "verify"
+            ? "Verify Account"
             : "Confirm";
 
     const iconToShow =
@@ -31,15 +37,23 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
             ? deactivateIcon
             : actionType === "resetPassword"
             ? resetIcon
+            : actionType === "reject"
+            ? rejectIcon
+            : actionType === "verify"
+            ? verifyIcon
             : deactivateIcon;
 
+    const isVerify = actionType === "verify";
+    const isReject = actionType === "reject";
     const isActivate = actionType === "activate";
 
-    const circleBg = isActivate ? "#10B935" : "#E21E26";
-    const glowShadow = isActivate
+    const circleBg = isVerify || isActivate ? "#10B935" : "#E21E26";
+    const glowShadow = isVerify || isActivate
         ? "0 25px 50px rgba(13,186,75,0.45)"
         : "0 25px 50px rgba(226,30,38,0.45)";
-    const shadeIcon = isActivate ? greenShadeIcon : redShadeIcon;
+
+    const shadeIcon =
+        isVerify || isActivate ? greenShadeIcon : redShadeIcon;
 
     return (
         <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
@@ -47,7 +61,7 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
 
                 <div
                     className={`relative h-52 w-full flex justify-center items-start pt-8 ${
-                        isActivate
+                        isVerify || isActivate
                             ? "bg-linear-to-b from-white via-white/40 to-[#e7f8ec]"
                             : "bg-linear-to-b from-white via-white/40 to-[#fdeeee]"
                     }`}
@@ -86,14 +100,14 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
                             <>
                                 <button
                                     onClick={onCancel}
-                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px] text-[15px] transition-all duration-200 hover:bg-yellow-500 hover:text-white"
+                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px]"
                                 >
                                     Cancel
                                 </button>
 
                                 <button
                                     onClick={onConfirm}
-                                    className="bg-[#FFCC00] text-black px-8 py-3 rounded-xl font-bold min-w-[200px] text-[15px] hover:bg-[#e6b800]"
+                                    className="bg-[#FFCC00] text-black px-8 py-3 rounded-xl font-bold min-w-[200px]"
                                 >
                                     Send Link
                                 </button>
@@ -104,21 +118,21 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
                             <>
                                 <button
                                     onClick={onCancel}
-                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px] text-[15px] transition-all duration-200 hover:bg-yellow-500 hover:text-white"
+                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px]"
                                 >
                                     Cancel
                                 </button>
 
                                 <button
                                     onClick={onConfirm}
-                                    className="bg-[#10B935] hover:bg-[#0aa33f] text-white px-8 py-3 rounded-xl font-semibold min-w-[200px] text-[15px]"
+                                    className="bg-[#10B935] text-white px-8 py-3 rounded-xl font-semibold min-w-[200px]"
                                 >
                                     Activate Account
                                 </button>
                             </>
                         )}
 
-                        {(actionType === "delete" || actionType === "deactivate") && (
+                            {(actionType === "delete" || actionType === "deactivate") && (
                             <>
                                 <button
                                     onClick={onConfirm}
@@ -132,6 +146,42 @@ function NotificationCard({ confirmModal, onConfirm, onCancel }) {
                                     className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px] text-[15px] transition-all duration-200 hover:bg-yellow-500 hover:text-white"
                                 >
                                     Cancel
+                                </button>
+                            </>
+                        )}
+
+                        {isReject && (
+                            <>
+                                <button
+                                    onClick={onCancel}
+                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px]"
+                                >
+                                    Cancel
+                                </button>
+
+                                <button
+                                    onClick={onConfirm}
+                                    className="bg-[#E21E26] text-white px-8 py-3 rounded-xl font-semibold min-w-[200px]"
+                                >
+                                    Reject Account
+                                </button>
+                            </>
+                        )}
+
+                        {isVerify && (
+                            <>
+                                <button
+                                    onClick={onCancel}
+                                    className="border-2 border-[#E5C100] text-[#E5C100] px-8 py-3 rounded-xl font-semibold min-w-[150px]"
+                                >
+                                    Cancel
+                                </button>
+
+                                <button
+                                    onClick={onConfirm}
+                                    className="bg-[#10B935] text-white px-8 py-3 rounded-xl font-semibold min-w-[200px]"
+                                >
+                                    Verify Account
                                 </button>
                             </>
                         )}
